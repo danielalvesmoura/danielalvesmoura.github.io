6e4c0c35db43f593389e5f24364b5ffca46b6215
danielalvesmoura <danielalvesmoura2006@gmail.com>
2026-04-16 20:19:04 -0300

    refactor-corrigindo-requisicao-do-github

--- js/github.js
+++ js/github.js
@@ -1,14 +1,11 @@
-const username = "danielalvesmoura";
 
-// 🔹 Perfil
-fetch(`https://api.github.com/users/${username}`).then(res => res.json()).then(data => {
+fetch(`https://api.github.com/users/danielalvesmoura`).then(res => res.json()).then(data => {
     document.getElementById("name").textContent = data.name || data.login;
     document.getElementById("followers").textContent = data.followers;
     document.getElementById("repos-count").textContent = data.public_repos;
 });
 
-// 🔹 Repositórios
-fetch(`https://api.github.com/users/${username}/repos`).then(res => res.json()).then(repos => {
+fetch(`https://api.github.com/users/danielalvesmoura/repos`).then(res => res.json()).then(repos => {
     const list = document.getElementById("repos");
 
     repos.forEach(repo => {
